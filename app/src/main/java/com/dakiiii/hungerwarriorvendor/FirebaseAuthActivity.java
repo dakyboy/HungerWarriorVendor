7cc840196425aae28005e24a805c28e58270816c
Aaron Dakiiii <eaaron25@gmail.com>
2020-11-05 05:49:39 +0300

    update firebase auth ui

--- app/src/main/java/com/dakiiii/hungerwarriorvendor/FirebaseAuthActivity.java
+++ app/src/main/java/com/dakiiii/hungerwarriorvendor/FirebaseAuthActivity.java
@@ -19,27 +19,42 @@ import java.util.List;
 public class FirebaseAuthActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 111;
-    public static Intent ordersIntent;
-    public static Intent mainIntent;
+    public static Intent sOrdersIntent;
+    public static Intent sMainIntent;
+    private FirebaseAuth eFirebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_auth);
-        ordersIntent = new Intent(FirebaseAuthActivity.this, OrdersActivity.class);
-        mainIntent = new Intent(FirebaseAuthActivity.this, MainActivity.class);
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
-        );
+        sOrdersIntent = new Intent(FirebaseAuthActivity.this, OrdersActivity.class);
+        sMainIntent = new Intent(FirebaseAuthActivity.this, MainActivity.class);
+        eFirebaseAuth = FirebaseAuth.getInstance();
 
-        startActivityForResult(
-                AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(),
-                RC_SIGN_IN
-        );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = eFirebaseAuth.getCurrentUser();
+        if (user != null) {
+            startActivity(sMainIntent);
+        } else {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build()
+            );
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN
+            );
+        }
+
     }
 
     @Override
@@ -51,9 +66,9 @@ public class FirebaseAuthActivity extends AppCompatActivity {
 
             if (resultCode == RESULT_OK) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                startActivity(mainIntent);
+                startActivity(sMainIntent);
                 Log.i("user is : ", user.getDisplayName());
-                Toast.makeText(this, user.getDisplayName(), Toast.LENGTH_SHORT).show();;
+                ;
             } else {
                 Log.i("user is :", "Not logged in");
                 Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show();
